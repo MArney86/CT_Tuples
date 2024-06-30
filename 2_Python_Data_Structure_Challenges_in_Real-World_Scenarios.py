49b0b87930d3cd27b856750649aacce081c6f5b6
MArney86 <matthew.h.arney.1986@gmail.com>
2024-06-30 14:53:08 -0500

    code for assignment 2

--- 2_Python_Data_Structure_Challenges_in_Real-World_Scenarios.py
+++ 2_Python_Data_Structure_Challenges_in_Real-World_Scenarios.py
@@ -1,8 +1,25 @@
-Task 1: Library System Enhancement Sharpen your skills in managing and modifying data within tuples and lists.
+#Task 1: Library System Enhancement Sharpen your skills in managing and modifying data within tuples and lists.
 
-Problem Statement: You are maintaining a library system where each book is stored as a tuple within a list. Your task is to update this system by adding new books and ensuring no duplicates.
+#Problem Statement: You are maintaining a library system where each book is stored as a tuple within a list. Your task is to update this system by adding new books and ensuring no duplicates.
 
-Existing Library Data:
+#- Add functionality to insert new books into `library`. Ensure that adding a duplicate book is handled appropriately.
+
+def add_book(library, title, author):
+    found = False
+    for book in library:
+        if book[0].lower() == title.lower() and book[1].lower() == author.lower():
+            found = True
+    if found:
+        print(f'Sorry "{title}" by {author} is already in the library.')
+    else:
+        library.append((title, author))
+        print(f'The title "{title}" by {author} was added to the library')
+
 
 library = [("1984", "George Orwell"), ("Brave New World", "Aldous Huxley")]
-- Add functionality to insert new books into `library`. Ensure that adding a duplicate book is handled appropriately.
+
+print(library)
+add_book(library, 'Jurrasic Park', 'Michael Chrichton')
+add_book(library, '1984', 'George Orwell')
+add_book(library, "Stranger From a Strange Land", "Robert A. Heinlein")
+print(library)
