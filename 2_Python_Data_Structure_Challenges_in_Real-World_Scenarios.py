a32d1916c8be48dee88c6a5fb631cebef3c7ac36
MArney86 <matthew.h.arney.1986@gmail.com>
2024-06-30 16:16:54 -0500

    commenting 2nd assignment

--- 2_Python_Data_Structure_Challenges_in_Real-World_Scenarios.py
+++ 2_Python_Data_Structure_Challenges_in_Real-World_Scenarios.py
@@ -4,22 +4,22 @@
 
 #- Add functionality to insert new books into `library`. Ensure that adding a duplicate book is handled appropriately.
 
-def add_book(library, title, author):
-    found = False
-    for book in library:
-        if book[0].lower() == title.lower() and book[1].lower() == author.lower():
-            found = True
-    if found:
-        print(f'Sorry "{title}" by {author} is already in the library.')
-    else:
-        library.append((title, author))
-        print(f'The title "{title}" by {author} was added to the library')
+def add_book(library, title, author): #function to add books to library
+    found = False #flag for checking if book to add is already in library
+    for book in library: #iterate through the books
+        if book[0].lower() == title.lower() and book[1].lower() == author.lower(): #check if titles and authors match in a case-independent manner
+            found = True #set flag to true when found
+    if found: #check the found flag
+        print(f'Sorry "{title}" by {author} is already in the library.') #notify user that that book already exists
+    else: #found flag is false
+        library.append((title, author)) #append a tuple with the title and author of the book to the library list
+        print(f'The title "{title}" by {author} was added to the library') #notify the user that the book was added to the list
 
 
-library = [("1984", "George Orwell"), ("Brave New World", "Aldous Huxley")]
+library = [("1984", "George Orwell"), ("Brave New World", "Aldous Huxley")] #initial library list
 
-print(library)
-add_book(library, 'Jurrasic Park', 'Michael Chrichton')
-add_book(library, '1984', 'George Orwell')
-add_book(library, "Stranger From a Strange Land", "Robert A. Heinlein")
-print(library)
+print(library) #print initial library
+add_book(library, 'Jurrasic Park', 'Michael Chrichton') #add book "Jurassic Park" by Michael Chrichton
+add_book(library, '1984', 'George Orwell') #add book "1984" by George Orwell
+add_book(library, "Stranger From a Strange Land", "Robert A. Heinlein") #add book "Stranger From a Strange Land" by Robert A. Heinlein
+print(library) #print updated library
